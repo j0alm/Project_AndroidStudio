90d3ebce941b70e42d7c7e5b2284ac862d198f2e
j0alm <joaoppalmeida1999@gmail.com>
2025-06-15 22:58:07 +0100

    Integração do GameViewModel com Compos no MainActivity
    - Implementado o uso do GameViewModel com viewModel() no MainActivity
    - Adicionada a dependência 'lifecycle-viewmodel-compose' ao build.gradle.kts
    - interfaceJogo agora recebe o ViewModel diretamente do MainActivity

--- app/src/main/java/com/example/gameproject/MainActivity.kt
+++ app/src/main/java/com/example/gameproject/MainActivity.kt
@@ -3,45 +3,19 @@ package com.example.gameproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.gameproject.ui.theme.GameProjectTheme
+import com.example.gameproject.ui.theme.interfaceJogo
+import com.example.gameproject.viewmodel.GameViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             GameProjectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val gameViewModel: GameViewModel = viewModel()
+                interfaceJogo(viewModel = gameViewModel)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GameProjectTheme {
-        Greeting("Android")
     }
 }
